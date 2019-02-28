147f634988187c40a6a6111b501d13aad100d0de
Ibrahim Keshta <keshta95@hotmail.com>
2019-02-28 15:14:54 +0200

    Fix env variables for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,10 @@
-var config = require("./config.json");
-// var envConfig = config[env]; // return an objects of the env
-// takes an object like envConfig return them as array
-Object.keys(config).forEach(key => {
-  process.env[key] = config[key];
-});
+var env = process.env.NODE_ENV || 'development' // only set on horuko
+
+if (env === 'development') {
+  var config = require("./config.json");
+  // var envConfig = config[env]; // return an objects of the env
+  // takes an object like envConfig return them as array
+  Object.keys(config).forEach(key => {
+    process.env[key] = config[key];
+  });
+}
